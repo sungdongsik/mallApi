aef16d6e57fbc02248a289535d52c271839ef3cb
성동식 <tjdehdtlr123@naver.com>
2024-05-11 23:46:09 +0900

    엔티티 CRUD 처리

--- src/main/java/org/zerock/mallapi/entity/Product.java
+++ src/main/java/org/zerock/mallapi/entity/Product.java
@@ -45,6 +45,11 @@ public class Product {
         this.pdesc = pdesc;
     }
 
+
+    public void changeDel(boolean delFlag){
+        this.delFlag = delFlag;
+    }
+
     public void addImage(ProductImage image){
         image.setOrd(imageList.size());
         imageList.add(image);
--- src/test/java/org/zerock/mallapi/repository/ProductRepositoryTest.java
+++ src/test/java/org/zerock/mallapi/repository/ProductRepositoryTest.java
@@ -4,8 +4,11 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
 import org.zerock.mallapi.entity.Product;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -31,5 +34,62 @@ class ProductRepositoryTest {
 
         productRepository.save(product);
 
+    }
+
+    @Test
+    @Transactional
+    public void testRead(){
+
+        Long pno = 1L;
+
+        Optional<Product> result = productRepository.findById(pno);
+
+        Product product = result.orElseThrow();
+
+        log.info(product);
+
+        log.info(product.getImageList());
+    }
+
+    @Test
+    public void testRead2(){
+
+        Long pno = 1L;
+
+        Optional<Product> result = productRepository.selectOnt(pno);
+
+        Product product = result.orElseThrow();
+
+        log.info(product);
+
+        log.info(product.getImageList());
+    }
+
+    @Commit
+    @Transactional
+    @Test
+    public void testDelete(){
+        Long pno = 2L;
+
+        productRepository.updateToDelete(pno, true);
+
+    }
+
+    @Test
+    public void testUpdate(){
+
+        Product product = productRepository.selectOnt(1L).get();
+
+        product.changePrice(3000);
+
+        product.clearList();
+
+        product.addImageString(UUID.randomUUID() + "_" + "PIMAGE1.jpg");
+
+        product.addImageString(UUID.randomUUID() + "_" + "PIMAGE2.jpg");
+
+        product.addImageString(UUID.randomUUID() + "_" + "PIMAGE3.jpg");
+
+        productRepository.save(product);
     }
 }
